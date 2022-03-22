a5f4c0ab4889f5333d9d7a098f6d4cd09209f128
jantapabh <jantapa2407@gmail.com>
2022-03-22 11:20:26 +0700

    first commit

---
+++ src/components/upload.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect} from 'react';
+import { storage } from "../firebase";
+import { ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
+
+const ReactFirebaseFileUpload = ({ data }) => {
+    
+  
+    return (
+      <div>
+        This one
+      </div>
+    );
+  };
+  
+export default ReactFirebaseFileUpload
